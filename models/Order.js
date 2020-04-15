646d1afad61fc5e2e4a15d012815c58462cd0b4d
SAIF0206 <msaif.sam@gmail.com>
2020-04-15 19:27:29 +0800

    Order Changes as Date

--- models/Order.js
+++ models/Order.js
@@ -25,7 +25,7 @@ module.exports = db.define(
       type: Sequelize.STRING,
     },
     FunctionDate: {
-      type: Sequelize.DATE,
+      type: Sequelize.STRING,
     },
     DeliveryTime: {
       type: Sequelize.DECIMAL,
--- routes/api/orders.js
+++ routes/api/orders.js
@@ -2,16 +2,39 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../config/Database");
 const Order = require("../../models/Order");
+const { Op } = require("sequelize");
+// const Sequelize = require("sequelize");
 
 // @route   GET /api/order/orderdata
 //@desc     Display Data
 //@access   Public
 router.get("/orderdata", (req, res) =>
-  Order.findAll()
+  Order.findAll({
+    where: {
+      FunctionDate: {
+        [Op.between]: [
+          req.body.FunctionDate + "T00:00:00.000Z",
+          req.body.FunctionDate + "T23:59:59.000Z",
+        ],
+      },
+    },
+  })
     .then((order) => {
-      console.log(Order);
       res.send(order).json;
     })
     .catch((err) => console.log("Error !!" + err))
 );
+
+// @route   GET /aip / order / display;
+//@desc     Display Data
+//@access   Public
+router.get("/display", (req, res) =>
+  Order.findAll()
+    .then((order) => {
+      console.log(Order.FunctionDate);
+      res.send(order).json;
+    })
+    .catch((err) => console.log("Error !!" + err))
+);
+
 module.exports = router;
